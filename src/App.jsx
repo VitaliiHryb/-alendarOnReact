677f8a3bd8bade9c1fb7a8b2237e81c3327c52e7
VitaliiHryb <vitaliigryb@ukr.net>
2022-09-15 20:05:46 +0300

    class ==> function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
 
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 
 import './common.scss';
+const App = () => {
+  const [weekStartDate, setWeekStartDate] = useState(new Date());
 
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+
+  return (
+    <>
+      <Header />
+      <Calendar weekDates={weekDates} />
+    </>
+  );
+};
+
+export default App;
+
+// old
+/*
 class App extends Component {
   state = {
     weekStartDate: new Date(),
@@ -23,5 +39,4 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+*/
